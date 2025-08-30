2dccc05ec267ec4a280ff283fbd9b8c5f12d9b75
dare-devil-ex <kmsedition@gmail.com>
2025-08-30 23:54:07 +0530

    modified:   config.json 	modified:   wkaie.py

--- wkaie.py
+++ wkaie.py
@@ -1,1 +1,15 @@
 # Author: @dare_devil_ex
+
+import json
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+token = config["token"]
+
+class Wkaie:
+    def wkaie():
+        print(token)
+
+
+if __name__=="__main__":
+    Wkaie.wkaie()
